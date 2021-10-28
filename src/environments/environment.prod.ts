f501dced7cadfb97bbd485c62abeb2edfd0fcc72
Administrator <dev@leadingsoft.eu>
2021-10-28 09:41:34 +0300

    commit-02

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyMyNZ3XiQN6u8qBduFgT-Njn-UW6sqZ8",
+    authDomain: "bookmark-landing-page-57c97.firebaseapp.com",
+    projectId: "bookmark-landing-page-57c97",
+    storageBucket: "bookmark-landing-page-57c97.appspot.com",
+    messagingSenderId: "417756173509",
+    appId: "1:417756173509:web:221155560078ebde5bccac",
+    measurementId: "G-Q5XK1V94CW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCyMyNZ3XiQN6u8qBduFgT-Njn-UW6sqZ8",
+    authDomain: "bookmark-landing-page-57c97.firebaseapp.com",
+    projectId: "bookmark-landing-page-57c97",
+    storageBucket: "bookmark-landing-page-57c97.appspot.com",
+    messagingSenderId: "417756173509",
+    appId: "1:417756173509:web:221155560078ebde5bccac",
+    measurementId: "G-Q5XK1V94CW"
+  }
 };
 
 /*
